4e6f3170a49a4656bc6f9db1ed1480394bdbcd33
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-09-04 12:58:52 +0600

    banner-left-right-done

--- Books/client/src/home/Home.jsx
+++ Books/client/src/home/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
+import Banner from "../components/Banner";
 
 const Home = () => {
   return (
-    <div className="">
-      <div className="h-screen">Home</div>
-      <div className="h-screen bg-red-700"></div>
-    </div>
+    <>
+      <Banner />
+    </>
   );
 };
 
