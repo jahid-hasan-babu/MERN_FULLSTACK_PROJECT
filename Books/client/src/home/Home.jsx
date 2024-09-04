1c1e493317e62908c04aeaa147472bef4b60bc70
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-09-04 12:19:28 +0600

    navbar-done

---
+++ Books/client/src/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="">
+      <div className="h-screen">Home</div>
+      <div className="h-screen bg-red-700"></div>
+    </div>
+  );
+};
+
+export default Home;
