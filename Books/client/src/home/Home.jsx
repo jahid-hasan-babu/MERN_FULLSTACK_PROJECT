91d36d8f3f5f5b5e45dfa753264315ec84459c63
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-09-06 09:53:38 +0600

    favorite-section-add

--- Books/client/src/home/Home.jsx
+++ Books/client/src/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner";
 import BestSellerBook from "./BestSellerBook";
+import FavBook from "./FavBook";
 
 const Home = () => {
   return (
     <>
       <Banner />
       <BestSellerBook />
+      <FavBook />
     </>
   );
 };
