73ee8ecaac21307397d70a02402d1bdf34dee236
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-09-06 11:04:26 +0600

    footer-add

--- Books/client/src/App.jsx
+++ Books/client/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import MyFooter from "./components/MyFooter";
 
 const App = () => {
   return (
     <>
       <NavBar />
-      <Outlet />
+      <div className="min-h-screen">
+        <Outlet />
+      </div>
+      <MyFooter />
     </>
   );
 };
