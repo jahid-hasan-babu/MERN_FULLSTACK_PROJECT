12d85a033a62652df6ca716a56e9a02234419bab
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2024-09-06 10:07:44 +0600

    other-book-and-promo-section-done

---
+++ Books/client/src/home/OtherBooks.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import BookCard from "../components/BookCard";
+
+const OtherBooks = () => {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8000/api/v1/all-book")
+      .then((res) => res.json())
+      .then((data) => setBooks(data.data.slice(4, 8)));
+  }, []);
+  return (
+    <div>
+      <BookCard books={books} headline="Other Books" />
+    </div>
+  );
+};
+
+export default OtherBooks;
